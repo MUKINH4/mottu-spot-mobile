97750115c1ecab9cbfece229bc3751d503b81680
MUKINH4 <samuelmaragato3@gmail.com>
2025-05-14 11:54:12 -0300

    list all motos screen

--- actions/fetchData.ts
+++ actions/fetchData.ts
@@ -17,3 +17,16 @@ export const getYards = async () => {
     throw error;
   }
 };
+
+export const getMotos = async () => {
+  try {
+    const response = await api.get("/moto")
+
+    if (response.status !== 200) {
+      throw new Error(`Erro na API: ${response.status}`)
+    }
+    return response.data.content;
+  } catch (error: any) {
+    console.error("Erro ao buscar pátios: ", error.message)
+  }
+}
--- app/motos.tsx
+++ app/motos.tsx
@@ -1,7 +1,61 @@
-import { Text } from "react-native";
+import { getMotos } from "@/actions/fetchData";
+import { AddMotoDTO } from "@/types/types";
+import { useEffect, useState } from "react";
+import { FlatList, ScrollView, StyleSheet, Text } from "react-native";
 
 export default function MotosScreen() {
+
+    const [data, setData] = useState<[]>([]);
+
+    const getAllMotos = async () => {
+        try {
+            const response = await getMotos()
+            setData(response)
+            return response
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        getAllMotos()
+    }, [])
+
+    function renderItem({item}: {item: AddMotoDTO}){
+        return (
+            <>
+            <ScrollView style={styles.card}>
+                <Text style={styles.cardTitle}>{item.placa}</Text>
+                <Text>Status: {item.status}</Text>
+                <Text>{item.descricao}</Text>
+                <Text>Patio: {item.patioId}</Text>
+            </ScrollView>
+            </>
+        )
+    }
+
     return (
-        <Text>Motos</Text>
+        <FlatList data={data} renderItem={renderItem} />
     )
 }
+
+const styles = StyleSheet.create({
+    card: {
+        backgroundColor: "#fff",
+        padding: 20,
+        borderRadius: 10,
+        marginBottom: 15,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+    },
+    cardTitle: {
+        fontSize: 18,
+        fontWeight: "bold",
+    }
+})
